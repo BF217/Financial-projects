90054f661cec7ca611dab84c3a82221a5dc469a8
BF217 <brendanfoottit@gmail.com>
2024-05-18 20:46:39 +0100

    NEW: project setup for frontend and backend.

---
+++ Backend/Financial-projects-api/src/main/java/com/financialprojectsapi/FinancialProjectsApiApplication.java
@@ -0,0 +1,13 @@
+package com.financialprojectsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialProjectsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialProjectsApiApplication.class, args);
+	}
+
+}
